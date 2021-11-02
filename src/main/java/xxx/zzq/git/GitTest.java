b40d5f24e29c71939a9ef593fb2080846fb48c6a
zzq <1250727176@qq.com>
2021-11-02 15:39:41 +0800

    second commit

--- src/main/java/xxx/zzq/git/GitTest.java
+++ src/main/java/xxx/zzq/git/GitTest.java
@@ -3,5 +3,6 @@ package xxx.zzq.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git2!");
     }
 }
